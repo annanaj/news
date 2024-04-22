9af8aa380add50861254252504cf7d1bb1dff081
Anna <65922372+annanaj@users.noreply.github.com>
2024-04-22 10:22:07 +0200

    Add grid to news articles

--- src/pages/NewsList/NewsList.tsx
+++ src/pages/NewsList/NewsList.tsx
@@ -40,10 +40,9 @@ export default function NewsList() {
 
 	return (
 		<div className="container mx-auto px-4">
-			<h1 className="text-2xl font-bold mb-4">News & Sentiment Trending</h1>
 
 			{loading && <div>
-				<div className="container mx-auto px-4">
+				<div className="container mt-24 mx-auto px-4">
 					<h1 className="text-2xl font-bold mb-4">News & Sentiment Trending</h1>
 					<p>Loading news...</p>
 				</div>
@@ -53,13 +52,16 @@ export default function NewsList() {
 				// <div>
 				// 	<pre>{JSON.stringify(newsData, null, 2)}</pre>
 				// </div>
-				newsData.map((news: NewsListProps, index: number) => (
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-24 ">
+					{newsData.map((news: NewsListProps, index: number) => (
 						<div key={index} className="bg-white rounded-lg shadow-lg p-6 mb-4">
-							<h2 className="text-xl font-bold mb-2">{news.title}</h2>
+							<h2 className="text-xl font-bold mb-2 text-gray-900">{news.title}</h2>
 							<p className="text-gray-700">{news.summary}</p>
-							<p className="text-gray-600 mt-2">Published on: {new Date(news.time_published).toLocaleDateString()}</p>
+							<p className="text-gray-500 text-sm mt-2">Published
+								on: {new Date(news.time_published).toLocaleDateString()}</p>
 						</div>
-					))
+					))}
+				</div>
 				) : (
 				<p>No news available as number of requests exceeded the daily limit</p>
 			)}
