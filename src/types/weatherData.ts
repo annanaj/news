7604ac6a4cb0a7420d734aabd02fec5d17663aaa
Anna <65922372+annanaj@users.noreply.github.com>
2024-05-31 15:42:22 +0200

    Add weather api

---
+++ src/types/weatherData.ts
@@ -0,0 +1,13 @@
+export interface WeatherData {
+	location: {
+		name: string,
+	},
+	current: {
+		temp_c: number,
+		condition: {
+			text: string,
+			icon: string,
+			code: number,
+		},
+	},
+}
