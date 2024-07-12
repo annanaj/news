273562edb7d7c3f77930bd94913fc59669eb845b
Anna <65922372+annanaj@users.noreply.github.com>
2024-07-12 10:33:52 +0200

    Adjust news fetch

--- src/hooks/useFetchNewsData.ts
+++ src/hooks/useFetchNewsData.ts
@@ -2,8 +2,8 @@ import useFetchData from './useFetchData';
 import { NewsData } from '../types/newsData';
 
 const apiKey = process.env.VITE_NEWS_API_KEY;
-const objects = ['NVDA', 'AAPL', 'MSFT'];
-const apiUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${objects.join(',')}&limit=10&apikey=${apiKey}`;
+const objects = ['AAPL'];
+const apiUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${objects.join(',')}&limit=1&apikey=${apiKey}`;
 
 export default function useFetchNewsData() {
 	const { data, loading, error } = useFetchData<NewsData[]>(apiUrl, 'cachedNewsData');
